c4207c015c274418181a199627b04a46232d5706
Jean Neiverth <jeanneiverth@gmail.com>
2024-12-17 16:27:34 -0300

    fix: base path config

--- farcaster-social-graph-api/farcaster_social_graph_api/config.py
+++ farcaster-social-graph-api/farcaster_social_graph_api/config.py
@@ -16,7 +16,7 @@ class Config:
         BASE_PATH = os.path.dirname(os.path.abspath(__file__))
         DATA_PATH = os.path.join(BASE_PATH, "../../data")
     else:
-        BASE_PATH = "/app/"
+        BASE_PATH = "/app"
         DATA_PATH = os.path.join(BASE_PATH, "data")
     DOWNLOAD_DATA_PATH = os.path.join(DATA_PATH, "raw")
     PERSISTED_DATA_PATH = os.path.join(DATA_PATH, "interim")
